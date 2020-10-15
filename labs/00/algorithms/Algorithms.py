c37d86afc73b3329c3ada6adcaa8860e8458873c
unknown <prokop.vojtech@gmail.com>
2020-10-15 20:37:39 +0200

    refactor: to abstract Ga

--- labs/00/algorithms/Algorithms.py
+++ labs/00/algorithms/Algorithms.py
@@ -295,16 +295,44 @@ class SimulatedAnnealingAlgorithm(AbstractAlgorithm):
             if self.graph:
                 self.graph.draw(self.best_solution, neighbourhood)
 
+class AbstractGeneticAlgorithm(AbstractAlgorithm):
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+
+    def copy(self, population):
+        return copy.deepcopy(population)
+
+
+    
+    def crossover(self, parent_A, parent_B):
+        length = len(parent_A.vector)
+        random_position_in_range = random.randint(0, length-1)
+        part_A = parent_A.vector[0:random_position_in_range, :]
+
+        rest = []
+        for city in parent_B.vector:
+            is_there = np.any(part_A == city)
+            if not is_there:
+                rest.append(list(city))
+
+        crossover_vector = np.concatenate((part_A, np.array(rest)), axis=0) 
+        offspring_AB = Solution()
+        offspring_AB.vector = crossover_vector
+        return offspring_AB
+    
+
+
 
 
 
 #TODO!: make abstract genetic algorithm, mutate etc..
-class GeneticAlgorithmTSP(AbstractAlgorithm):
+class GeneticAlgorithmTSP(AbstractGeneticAlgorithm):
     def __init__(self, number_of_cities=20, low =0, high=200, **kwds):
         """
             NP = size of population
             G = max_generation
 
+            CONVENCTION!!!
         Args:
             number_of_cities (int): D, it will be a number of cities
         """
@@ -331,31 +359,9 @@ class GeneticAlgorithmTSP(AbstractAlgorithm):
     def generate_population(self, cities):
         """Generating NP random individuals
         """
+        super().generate_population()
         population = [self.generate_individual(cities) for _ in range(self.size_of_population)]
         return population
-
-    def copy(self, population):
-        return copy.deepcopy(population)
-
-    def crossover(self, parent_A, parent_B):
-
-        length = len(parent_A.vector)
-        random_position_in_range = random.randint(0, length-1)
-        part_A = parent_A.vector[0:random_position_in_range, :]
-
-        rest = []
-        for city in parent_B.vector:
-            is_there = np.any(part_A == city)
-            if not is_there:
-                rest.append(list(city))
-
-        crossover_vector = np.concatenate((part_A, np.array(rest)), axis=0) 
-        offspring_AB = Solution()
-        offspring_AB.vector = crossover_vector
-        return offspring_AB
-    
-
-
 
     def mutate(self, offspring_AB):
         first_index = int(np.random.uniform(0, len(offspring_AB.vector)))
