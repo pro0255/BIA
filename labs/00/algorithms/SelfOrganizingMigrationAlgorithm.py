82a449caa2bb29cb2dd50d2108f86391d75e6e2a
unknown <prokop.vojtech@gmail.com>
2020-11-04 22:23:54 +0100

    📝 docs(soma): finished

--- labs/00/algorithms/SelfOrganizingMigrationAlgorithm.py
+++ labs/00/algorithms/SelfOrganizingMigrationAlgorithm.py
@@ -12,11 +12,23 @@ class SelfOrganizingMigrationAlgorithm(AbstractGeneticAlgorithm):
         super().__init__(**kwds)
 
     def generate_population(self, Function):
+        """ Generates init population for alg. Individuals rand from random uniform.
+        Args:
+            Function (Function): Sphere Ackley..
+        Returns:
+            [Solution[]]: Generated population as array of Solutin class. 
+        """
         return [
             self.generate_individual(Function) for i in range(self.size_of_population)
         ]
 
     def generate_individual(self, Function):
+        """ Generates single individual. Method is used in generated population.
+        Args:
+            Function (Function): Sphere Ackley..
+        Returns:
+            [Solution]: Generated Solution class, where vector of parameters is populated with random uniform values in bounderies. 
+        """
         return self.generate_random_solution(Function.left, Function.right)
 
     def generate_PRTVector(self, d=2):
@@ -30,17 +42,37 @@ class SelfOrganizingMigrationAlgorithm(AbstractGeneticAlgorithm):
         return np.where(PRTVector < self.PRT, 0, 1)
 
     def calculate_position(self, a, b, PRTVector, step):
+        """ Generates new positon according to formula.
+        Args:
+            a (float[]): Start position.
+            b (float[]): Directional vector. B - A formula.
+            PRTVector (float[]): Generated PRTVector accroding to formula. Populated with [0, 1].
+            step ([float): Current step.
+        Returns:
+            [float[]]: New position.
+        """
         return a + (b - a)*step*PRTVector
 
     def get_pair_all_to_one(self):
+        """ Returns vector when is used AllToOne strategy.
+        Returns:
+            [float[]]: Vector with best fitness value. It is selected from population.
+        """
         return self.best_solution.vector
 
     def individual_path_all_to_one(self, solution, get_pair):
+        """ Runs step logic. Where for individual is generated set of new positions. Tries to find global extrem.
+        Args:
+            solution (Solution): Current individual.
+            get_pair (func): Method which returns vector. It can be used in implementation fo more strategies. [AllToAllAdaptive, AllToOneRand..]
+        Returns:
+            [Solution[]]: Populated vector with solutions.
+        """
         path_solutions = []
         t = 0
         first_vector = solution.vector
         while t < self.path_length:
-            second_vector = self.best_solution.vector
+            second_vector = get_pair()
             new_position = self.calculate_position(first_vector, second_vector, self.generate_PRTVector(solution.dimension), t)
             new_position = np.clip(new_position, solution.lower_bound, solution.upper_bound) 
             new_solution = copy.deepcopy(solution)
@@ -50,7 +82,15 @@ class SelfOrganizingMigrationAlgorithm(AbstractGeneticAlgorithm):
         return path_solutions
 
 
-    def crossover(self, solution, Function):    
+    def crossover(self, solution, Function):
+        """ Method which selects for current input individual solution with best fitness. New one || old is returned according this selection.
+            Also draw search to graph a replaces values from graph.
+        Args:
+            solution (Solution): Current individual.
+            Function (Function): Sphere, Ackley..
+        Returns:
+            [Solution]: Best solution on all generated solutions for specific individual.
+        """
         possible_solutions = self.individual_path_all_to_one(solution, self.get_pair_all_to_one)
         self.evalute_population(possible_solutions, Function)
         best = self.select_best_solution(possible_solutions)
@@ -59,6 +99,13 @@ class SelfOrganizingMigrationAlgorithm(AbstractGeneticAlgorithm):
         return best 
     
     def terminate_parameters_check(self, population):
+        """ At the of algorithm checks parameters which can terminate algorithm earlier.
+            Selects best and worst, makes diff, abs and check (textbooks error??).
+        Args:
+            population (Solution[]): Current population.=
+        Returns:
+            [boolean]: According to condition return boolean. If true then terminate.
+        """
         best = self.select_best_solution(population)
         worst = self.select_worst_solution(population)
         diff = abs(best.fitness_value - worst.fitness_value)
