5bf7401cff635eab188a78ca990814d6738111f5
unknown <prokop.vojtech@gmail.com>
2020-11-26 19:20:00 +0100

    docs: finished

--- labs/00/algorithms/TeachingLearningBasedOptimization.py
+++ labs/00/algorithms/TeachingLearningBasedOptimization.py
@@ -29,6 +29,11 @@ class TeachingLearningBasedAlgorithm(AbstractGeneticAlgorithm):
         return self.generate_random_solution(Function.left, Function.right)
 
     def learning_phase(self, Function, students):
+        """ Represents learning phase in algorithm, where teachers ala best solution moves mean forward.
+        Args:
+            Function (Function): Sphere Ackley..
+            students (Solution[]): Current population, group of students.
+        """
         self.evalute_population(students, Function)
         self.best_solution = self.select_best_solution(students)
         new_position = np.zeros(self.best_solution.dimension)
@@ -39,14 +44,32 @@ class TeachingLearningBasedAlgorithm(AbstractGeneticAlgorithm):
             self.best_solution.vector = new_position 
 
     def get_random_number(self):
+        """ Generates random number in uniform distribution (0,1) interval.
+        Returns:
+            [float]: Random uniform values.
+        """
         return np.random.uniform()     
 
     def calculate_difference(self, Function, students):
+        """Calculates difference according to mean, random value uniform and random value tf.
+        Args:
+            Function (Function): Sphere Ackley..
+            students (Solution[]): Whole pop represents students with teacher.
+        Returns:
+            [float[]]: Vector with in D.
+        """
         r = self.get_random_number()
         Tf = np.random.randint(1, 3)
         return r * (self.best_solution.vector - Tf*self.calculate_mean(students))
 
     def get_random_student(self, to, students):
+        """Get random student according to current one. Conditions is that i != j!
+        Args:
+            to (Solution): Current student.
+            students (Solution[]): Whole pop represents students with teacher.
+        Returns:
+            [Solution]: Picked student from pop fulfilling condition.
+        """
         random_s = random.choice(students)
         if random_s.key == to.key:
             return self.get_random_student(to, students)
@@ -54,6 +77,11 @@ class TeachingLearningBasedAlgorithm(AbstractGeneticAlgorithm):
             return random_s
 
     def learners_phase(self, Function, students):
+        """Represents second phase in algorithm. Where every student learns from other one. Teacher included.
+        Args:
+            Function (Function): Sphere Ackley..
+            students (Solution[]): Whole pop represents students with teacher.
+        """
         for student in students:
             random_s = self.get_random_student(student, students)
             new_vector = np.zeros(student.dimension)
@@ -69,12 +97,18 @@ class TeachingLearningBasedAlgorithm(AbstractGeneticAlgorithm):
                 self.evaluate(student, Function)
 
     def calculate_mean(self, students):
+        """Calculates mean in class.
+        Args:
+            students (Solution[]): Whole pop represents students.
+        Returns:
+            [type]: Mean in class
+        """
         matrix = [student.vector for student in students]
         return np.mean(np.squeeze(matrix), axis=0)
 
 
     def start(self, Function):
-        """Runs ______ Algorithm on specified Function, with specified args.
+        """Runs TeachingLearning Algorithm on specified Function, with specified args.
         Args:
             Function (class Function): specific Function (Sphere || Ackley..)
         """
