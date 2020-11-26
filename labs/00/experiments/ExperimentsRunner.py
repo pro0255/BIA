fa51d949132df79024749dd90b5996f41596d88a
unknown <prokop.vojtech@gmail.com>
2020-11-27 00:36:30 +0100

    ✅ feat: experiments finished

--- labs/00/experiments/ExperimentsRunner.py
+++ labs/00/experiments/ExperimentsRunner.py
@@ -1,17 +1,10 @@
 from experiments import EXPERIMENT_CONSTANTS
-from algorithms.DifferentialEvolution import DifferentialEvolutionAlgorithm
-from algorithms.ParticleSwarmOptimization import ParticleSwarmOptimizationAlgorithm
-from algorithms.SelfOrganizingMigrationAlgorithm import SelfOrganizingMigrationAlgorithm
-from algorithms.FireflyAlgorithm import FireflyAlgorithm
-from algorithms.TeachingLearningBasedOptimization import TeachingLearningBasedAlgorithm
-from Graph import Graph
-from experiments.DELIMITER import DELIMITER
+from experiments.DELIMITER import DELIMITER, DELIMITER_README
 from experiments.EXPERIMENTS_OUTPUT import EXPERIMENTS_PATH
 import os
 import time
 import pandas as pd
-
-#TODO: can save in same file? as other tab in excel? :D
+import numpy as np
 
 SAVE = True
 
@@ -23,6 +16,7 @@ class ExperimentsRunner():
         self.NP = NP
         self.MAX_G = MAX_G
         self.N_O_G = NUMBER_OF_EXPERIMENTS
+        self.results = {}
 
     def set_properties(self, algorithms):
         for a in algorithms:
@@ -34,50 +28,96 @@ class ExperimentsRunner():
         self.set_properties(algs)
         return algs
 
+
+    def create_index(self, l):
+        return [f'Experiment {i+1}' for i in range(l)]
+
+    def make_df_calculations(self):
+        for k, v in self.results.items():
+            means = {}
+            std_devs = {}
+            for column in v:
+                values = v[column].values
+                mean = np.mean(v[column].values)
+                std_dev = np.std(v[column].values)
+                means[column] = mean
+                std_devs[column] = std_dev
+            new_df = v.copy()
+            new_df.loc['mean'] = list(means.values())
+            new_df.loc['std_dev'] = list(std_devs.values()) 
+            self.results[k] = new_df
+            print(self.results[k])
+    def save_to_xls(self, name):
+        self.check_save()
+        file_name = f'{EXPERIMENTS_PATH}//{name}.xlsx'
+        print(f'Saving to {file_name}..')
+        with pd.ExcelWriter(file_name, mode='w') as writer:
+            for k,v in self.results.items():
+                v.to_excel(writer, sheet_name=k)
+
+
+    def save_data_frame(self, df, func):
+        self.results[func] = df
+
+    def create_experiment_dataframe(self, dic, func):
+        df = pd.DataFrame(dic)
+        df.index = self.create_index(len(df))
+        self.save_data_frame(df, func)
+        # df.index.names = self.create_index(len(df))
+
+    def save_read_me(self):
+        output = [f'{DELIMITER_README}{str(alg)}{DELIMITER_README}' for alg in EXPERIMENT_CONSTANTS.ALGORITHMS_TO_RUN]
+        self.save_to_file(f'\n'.join(output), "README", '.txt')
+
     def start_experiments_for_functions(self, functions=EXPERIMENT_CONSTANTS.FUNCTION_TO_RUN):
-        save_read_me = True
+        self.results = {}
         start = time.time()
+
         print('Starting experiments for specified functions.')
         for f in functions:
-            self.start_experiments(f, save_read_me)
-            save_read_me = False
+            output_dic = self.start_experiments(f)
+            self.create_experiment_dataframe(output_dic, type(f).__name__)
         print('Thanks for your time experiments finished :-].')
+
         end = time.time()
         how_many = str(end - start)
-        self.save_experiment(f"Time to calculate {how_many}", "INFO", ".txt")
-        
 
-    def start_experiments(self, function, save_read_me):
-        read_me = ""
-        csv = ""
+        self.save_read_me()
+        self.make_df_calculations()
+        exit()
+        if SAVE:
+            self.save_to_file(f"Time to calculate {how_many}", "INFO", ".txt")
+            self.save_to_xls("experiments")
+            
+
+    def start_experiments(self, function):
+        dic = {self.get_algorithm_key(alg):[] for alg in EXPERIMENT_CONSTANTS.ALGORITHMS_TO_RUN}
         FUNCTION_NAME = type(function).__name__
         print(f'Starting for objective function *{FUNCTION_NAME}* ')
         for i in range(self.N_O_G):
             transformed_index = i + 1 #lol
-            fV, desription = self.start_experiment(function)
-            if not i:
-                read_me = DELIMITER.join(desription)
-            csv += f'Experiment {transformed_index}{DELIMITER}{DELIMITER.join([str(f) for f in fV])}\n'
-        if SAVE:
-            self.save_experiment(csv, FUNCTION_NAME.lower())
-            if save_read_me:
-                self.save_experiment(read_me, 'README', '.txt')
+            self.start_experiment(function, dic)
+        return dic
 
-    def start_experiment(self, function):
-        description_algorithms = []
-        fV_algoritms = []
+
+    def get_algorithm_key(self, algorithm):
+        return type(algorithm).__name__
+
+    def start_experiment(self, function, dic):
         for algorithm in self.build():
             fV, description = algorithm.start(function)
-            description_algorithms.append(description)
-            fV_algoritms.append(fV)
-        return (fV_algoritms, description_algorithms)
+            dic[self.get_algorithm_key(algorithm)].append(fV)
 
-
-    def save_experiment(self, text, name, suffix='-experiments.csv', directory="experiments"):
+    def check_save(self, path=EXPERIMENTS_PATH):
         path = f'{EXPERIMENTS_PATH}'
         if not os.path.exists(path):
             os.makedirs(path)
-        with open(f'{path}//{name}{suffix}', 'w') as f:
+
+
+
+    def save_to_file(self, text, name, suffix='-experiments.csv', directory="experiments"):
+        self.check_save()
+        with open(f'{EXPERIMENTS_PATH}//{name}{suffix}', 'w') as f:
             f.write(text)
 
         
---
+++ labs/00/experiments/output/README.txt
@@ -0,0 +1,11 @@
+
+==========================================================================
+Starts algorithm DifferentialEvolutionAlgorithm with attributes:
+	crossover_range -- 0.5
+	mutation_constant -- 0.5
+	size_of_population -- 30
+	max_generation -- 100
+	graph -- None
+	index_of_generation -- 100
+	D -- 30
+==========================================================================
