cd48649d97a718083fd8d435a47758200759a86a
unknown <prokop.vojtech@gmail.com>
2020-11-29 17:02:55 +0100

    chore: app

--- labs/00/main.py
+++ labs/00/main.py
@@ -5,12 +5,12 @@ from experiments.ExperimentsRunner import ExperimentsRunner
 """
 Starts GUI with selection of alg, function and hyperparameters.
 """
-# app = Application()
-# app.start(False)
+app = Application()
+app.start(False)
 
 
 """
 Class which start experiments and save them to file. It is possible to start it again with specified constants value. These are situated in experiments/EXPERIMENTS_CONSTANTS.py
 """
-exp = ExperimentsRunner()
-exp.start_experiments_for_functions()
+# exp = ExperimentsRunner()
+# exp.start_experiments_for_functions()
